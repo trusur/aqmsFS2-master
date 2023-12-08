7703acbbdd763012c32a01f5ece1fceb1502ef76
ir001 <irwanantonio2708@gmail.com>
2023-12-08 15:51:13 +0700

    fix: update routes

--- gui/app/Config/Routes.php
+++ gui/app/Config/Routes.php
@@ -3,6 +3,31 @@
 use CodeIgniter\Router\RouteCollection;
 
 /**
+ * --------------------------------------------------------------------
+ * Router Setup
+ * --------------------------------------------------------------------
+ */
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Home');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+$routes->setAutoRoute(true);
+
+/**
  * @var RouteCollection $routes
  */
+
+// We get a performance increase by specifying the default
+// route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('lang/(:any)', 'Language::index');
+$routes->get('configurations', 'Configuration::index');
+$routes->get('parameters', 'Parameter::index');
+$routes->get('calibrations', 'Calibration::index');
+$routes->get('calibration/zero/{:num}', 'Calibration::zero');
+$routes->get('calibration/span/{:num}', 'Calibration::span');
+$routes->get('calibration/datatable', 'Calibration::datatable');
+$routes->get('exports', 'Export::index');
+
+$routes->add('/switch/pump', 'Home::pump');
