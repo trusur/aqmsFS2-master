b472d765d30ae9e34687dcd37c69161f85f4bfe8
ir001 <irwanantonio2708@gmail.com>
2023-12-09 15:54:28 +0700

    fix: config

--- db_connect.py
+++ db_connect.py
@@ -3,6 +3,6 @@ import mysql.connector
 
 def connecting():
     try:
-        return mysql.connector.connect(host="localhost",user="root",passwd="R2h2s12*",database="aqms_fs2")
+        return mysql.connector.connect(host="localhost",user="root",passwd="R2h2s12*",database="aqms_efs1")
     except Exception as e: 
-        return false
+        return False
