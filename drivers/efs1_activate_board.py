7e26b83d8af3297c72fb967eab5364ed196de458
ir001 <irwanantonio2708@gmail.com>
2023-12-13 12:28:14 +0700

    feat: activate semeatech

---
+++ drivers/efs1_activate_board.py
@@ -0,0 +1,20 @@
+from pyModbusTCP.client import ModbusClient
+import logging
+def connect_motherboard():
+    try:
+        host = "192.168.123.150"
+        port = 502
+        return ModbusClient(host=host, port=port, debug=False, timeout=5, unit_id=1, auto_open=True, auto_close=False)
+    except Exception as e:
+        logging.error("Motherboard connection error: " + str(e))
+        return False
+    
+def main():
+        board = connect_motherboard()
+        if board:
+            board.open()
+            # Activate Semeatech
+            board.write_single_register(303,1)
+            print(board.read_input_registers(303,1))
+        else:
+            print("Motherboard connection error")
