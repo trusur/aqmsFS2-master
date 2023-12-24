8626f8ecca027255bfdfe92031463cdf8532216a
ir001 <irwanantonio2708@gmail.com>
2023-12-25 03:20:24 +0700

    fix: bool return in db_connect

--- drivers/db_connect.py
+++ drivers/db_connect.py
@@ -5,4 +5,4 @@ def connecting():
     try:
         return mysql.connector.connect(host="localhost",user="root",passwd="R2h2s12*",database="aqms_efs1")
     except Exception as e: 
-        return false
+        return False
