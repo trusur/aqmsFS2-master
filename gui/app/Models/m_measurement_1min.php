a78f522bc019c7ee743694c1b71f6504f535a547
ir001 <irwanantonio2708@gmail.com>
2024-02-29 14:55:20 +0700

    fix: measurement1mins table

--- gui/app/Models/m_measurement_1min.php
+++ gui/app/Models/m_measurement_1min.php
@@ -7,7 +7,7 @@ use CodeIgniter\Model;
 class m_measurement_1min extends Model
 {
     protected $DBGroup = 'default';
-    protected $table = 'measurement_1min';
+    protected $table = 'measurement_1mins';
     protected $primaryKey = 'id';
     protected $returnType = 'object';
     protected $useTimestamps = false;
