a7f6eb577538cb5f71ec39e9bf820138aea18bc7
ir001 <irwanantonio2708@gmail.com>
2024-03-27 14:17:40 +0700

    add: convert to myisam

---
+++ gui/app/Database/Migrations/2024-03-27-071340_AlterToMyISAM.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\CLI\CLI;
+use CodeIgniter\Database\Migration;
+use Exception;
+
+class AlterToMyISAM extends Migration
+{
+    public function up()
+    {
+        $tables=[
+            "configurations",
+            "measurements",
+            "measurement_logs",
+            "measurement_1mins",
+            "realtime_values",
+            "sensor_value_logs",
+            "log_sent",
+            "parameters",
+            "motherboard",
+        ];
+        $db = db_connect();
+        foreach ($tables as $table) {
+            try{
+                $db->query("ALTER TABLE {$table} ENGINE='MyISAM';");
+            }catch(Exception $e){
+                CLI::write($e->getMessage(),"red");
+            }
+        }
+    }
+
+    public function down()
+    {
+        //
+    }
+}
