34ff2426a80c51c7a99d1180f77fe792763cdf5f
ir001 <irwanantonio2708@gmail.com>
2024-06-20 14:35:51 +0700

    test modbus

--- drivers/test.py
+++ drivers/test.py
@@ -1,7 +1,27 @@
-response = "SEMEATECH START;SEMEATECH 0x01;NO2;58;0.002;28.50;79;END;SEMEATECH 0x02;SO2;58;0.002;28.50;79;END;SEMEATECH 0x03;O3;58;0.002;28.50;79;END;SEMEATECH 0x04;CO;58;0.002;28.50;79;END;SEMEATECH FINISH;"
-sematech = response.split(";END;")
-for index,res in enumerate(sematech):
-    final_str = res.replace("SEMEATECH START;","")
-    final_str = final_str.replace("SEMEATECH FINISH;","")
-    print(final_str)
-# print(sematech)
+from pymodbus.client import ModbusSerialClient
+from pymodbus.client import ModbusTcpClient
+# client = ModbusSerialClient(
+#     method="rtu",
+#     port="/dev/tty.usbserial-1140",
+#     baudrate=9600,
+#     bytesize=8,
+#     parity="N",
+#     stopbits=1,
+#     timeout=1,
+#     strict=True
+# )
+client = ModbusTcpClient(
+    host="127.0.0.1",
+    port=502,
+    timeout=1
+)
+
+print(client.connect())
+print(client.write_coil(address=1,value=True))
+init_address = 2048
+for i in range(99):
+    print(client.write_coil(address=init_address+i,value=False))
+    read_coils = client.read_coils(address=init_address)
+    print(read_coils.bits[0])
+# print(client.read_coils(address=1,slave=1).registers)
+# print(client.read_input_registers(address=1).registers)
