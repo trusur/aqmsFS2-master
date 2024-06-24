dd0e1cf0d428b574864ea6e32887230a797cfa4e
ir001 <irwanantonio2708@gmail.com>
2024-06-24 14:33:22 +0700

    refactor: sending data

--- gui/app/Commands/Sentdata.php
+++ gui/app/Commands/Sentdata.php
@@ -10,6 +10,7 @@ use App\Models\m_parameter;
 use App\Models\m_sensor_value;
 use CodeIgniter\CLI\BaseCommand;
 use CodeIgniter\CLI\CLI;
+use Exception;
 
 class Sentdata extends BaseCommand
 {
@@ -77,83 +78,89 @@ class Sentdata extends BaseCommand
 	 */
 	public function run(array $params)
 	{
-		$is_sentto_trusur = @$this->configurations->where("name", "is_sentto_trusur")->first()->content ?? "1";
-		if ($is_sentto_trusur == "1") {
-			$trusur_api_server = @$this->configurations->where("name", "trusur_api_server")->first()->content ?? "";
-			$measurement_ids = "";
-			$arr["id_stasiun"] = @$this->configurations->where("name", "id_stasiun")->first()->content ?? null;
+		$is_sentto_trusur = get_config("is_sentto_trusur");
+		if($is_sentto_trusur != 1){
+			CLI::write("Pengiriman tidak diaktifkan", "yellow");
+			return;
+		}
+		$arr["id_stasiun"] = get_config("id_stasiun") ?? null;
+		$time_groups = $this->measurements->select("time_group")->where(["is_sent_cloud" => 0])->groupBy("time_group")->orderBy("id")->findAll();
+		foreach ($time_groups as $timeGroup) {
+			$time_group = $timeGroup->time_group;
+			CLI::write("Sent data {$time_group} to Trusur Server", "green");
+			$arr["waktu"] = $time_group;
+			$measurements = @$this->measurements->where(["time_group" => $time_group, "is_sent_cloud" => 0])->orderBy("id")->findAll();
+			foreach ($measurements as $measurement) {
+				$parameter = @$this->parameters->where(["id" => $measurement->parameter_id])->first();
+				if(empty($parameter)) continue;
 
-			$time_groups = @$this->measurements->select("time_group")->where(["is_sent_cloud" => 0])->groupBy("time_group")->orderBy("id")->findAll();
-			foreach ($time_groups as $timeGroup) {
-				$time_group = $timeGroup->time_group;
-				CLI::write("Sent data {$time_group} to Trusur Server", "green");
-				$is_exist = false;
-				if ($time_group) {
-					$is_exist = true;
-					$arr["waktu"] = $time_group;
-					$measurements = @$this->measurements->where(["time_group" => $time_group, "is_sent_cloud" => 0])->orderBy("id")->findAll();
-					foreach ($measurements as $measurement) {
-						$parameter = @$this->parameters->where(["id" => $measurement->parameter_id])->first();
-						$arr[$parameter->code] = $measurement->value;
-						if($measurement->avg_id){
-							$arr["avg_id"] = $measurement->avg_id;
-						}
-						if($parameter->p_type == "particulate" || $parameter->p_type == "gas"){
-							$arr["stat_{$parameter->code}"] = $measurement->is_valid;
-							$arr["total_{$parameter->code}"] = $measurement->total_data;
-							$arr["valid_{$parameter->code}"] = $measurement->total_valid;
-							
-						}
-						$measurement_ids .= $measurement->id . ",";
-					}
+				$arr[$parameter->code] = $measurement->value;
+				if($measurement->avg_id){
+					$arr["avg_id"] = $measurement->avg_id;
 				}
-				$measurement_ids = substr($measurement_ids, 0, -1);
-				if ($is_exist) {
-					$trusur_api_username = @$this->configurations->where("name", "trusur_api_username")->first()->content ?? "";
-					$trusur_api_password = @$this->configurations->where("name", "trusur_api_password")->first()->content ?? "";
-					$trusur_api_key = @$this->configurations->where("name", "trusur_api_key")->first()->content ?? "";
-					$data = json_encode($arr);
-					$curl = curl_init();
-					curl_setopt_array($curl, array(
-						CURLOPT_URL => "https://" . $trusur_api_server . "/api/put_data.php",
-						CURLOPT_RETURNTRANSFER => true,
-						CURLOPT_ENCODING => "",
-						CURLOPT_MAXREDIRS => 10,
-						CURLOPT_TIMEOUT => 30,
-						CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-						CURLOPT_CUSTOMREQUEST => "PUT",
-						CURLOPT_USERPWD => $trusur_api_username . ":" . $trusur_api_password,
-						CURLOPT_POSTFIELDS => $data,
-						CURLOPT_HTTPHEADER => array(
-							"Api-Key: " . $trusur_api_key,
-							"cache-control: no-cache",
-							"content-type: application/json"
-						),
-						CURLOPT_SSL_VERIFYPEER => 0, //skip SSL Verification | disable SSL verify peer
-					));
-
-					$response = curl_exec($curl);
-					$err = curl_error($curl);
-
-					curl_close($curl);
-
-					if ($err) {
-						echo "cURL Error #:" . $err;
-					} else {
-						if (strpos(" " . $response, "success") > 0) {
-							$this->measurements->where(["time_group" => $time_group])->set(["is_sent_cloud" => 1, "sent_cloud_at" => date("Y-m-d H:i:s")])->update();
-						} else {
-							echo $response;
-						}
-					}
+				if($parameter->p_type == "particulate" || $parameter->p_type == "gas"){
+					$arr["stat_{$parameter->code}"] = $measurement->is_valid;
+					$arr["total_{$parameter->code}"] = $measurement->total_data;
+					$arr["valid_{$parameter->code}"] = $measurement->total_valid;
 				}
+				$this->sendData($arr, $time_group);
 			}
-			
 		}
 		// Check if all data 1sec has been sent
 		$totalData1sec = $this->MLogSent->countAllResults();
 		if ($totalData1sec == 0 || $totalData1sec > 2100) {
 			$this->MLogSent->truncate();
+		}
+	}
+
+	public function sendData($data, $time_group){
+		try{
+			$trusur_api_server = get_config("trusur_api_server");
+			$trusur_api_username = get_config("trusur_api_username");
+			$trusur_api_password = get_config("trusur_api_password");
+			$trusur_api_key = get_config("trusur_api_key");
+			$data = json_encode($data);
+			$curl = curl_init();
+			curl_setopt_array($curl, array(
+				CURLOPT_URL => "https://" . $trusur_api_server . "/api/put_data.php",
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => "",
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 30,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => "PUT",
+				CURLOPT_USERPWD => $trusur_api_username . ":" . $trusur_api_password,
+				CURLOPT_POSTFIELDS => $data,
+				CURLOPT_HTTPHEADER => array(
+					"Api-Key: " . $trusur_api_key,
+					"cache-control: no-cache",
+					"content-type: application/json"
+				),
+				CURLOPT_SSL_VERIFYPEER => 0, //skip SSL Verification | disable SSL verify peer
+			));
+
+			$response = curl_exec($curl);
+			$err = curl_error($curl);
+
+			curl_close($curl);
+
+			if ($err) {
+				echo "cURL Error #:" . $err;
+				CLI::write("[Error] - $err", "red");
+				return false;
+			} else {
+				if (strpos(" " . $response, "success") > 0) {
+					$this->measurements->where(["time_group" => $time_group])->set(["is_sent_cloud" => 1, "sent_cloud_at" => date("Y-m-d H:i:s")])->update();
+					CLI::write("[Success]  -Sent data {$time_group} to Trusur Server", "green");
+					return true;
+				} else {
+					CLI::write("[Error] - Sending data {$time_group} to Trusur Server", "red");
+					print_r($response);
+					return false;
+				}
+			}
+		}catch(Exception $e){
+			return false;
 		}
 	}
 }
