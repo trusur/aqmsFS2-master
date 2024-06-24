bbf2396c4a179428fc084b4f4346d1161106c6ba
ir001 <irwanantonio2708@gmail.com>
2024-06-24 17:42:39 +0700

    fix: disable sleep

--- services/averaging.sh
+++ services/averaging.sh
@@ -5,5 +5,4 @@ while true
         /usr/bin/php /home/mx/aqms-efs2/gui/spark command:formula_measurement_logs
         /usr/bin/php /home/mx/aqms-efs2/gui/spark command:avg1min
         /usr/bin/php /home/mx/aqms-efs2/gui/spark command:avg30min
-        sleep 1
     done
