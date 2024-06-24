8bb32aedb783969dfec9c202c9546ec526df4dab
ir001 <irwanantonio2708@gmail.com>
2024-06-24 15:44:01 +0700

    fix: python3 path

--- services/driver.sh
+++ services/driver.sh
@@ -2,6 +2,6 @@
 while true
     do
         # Replace drivers with full path to drivers folder
-        python  /home/mx/aqms-efs2/drivers/pump_efs2.py
-        python  /home/mx/aqms-efs2/drivers/mainboard_efs2.py
+        /usr/bin/python3  /home/mx/aqms-efs2/drivers/pump_efs2.py
+        /usr/bin/python3  /home/mx/aqms-efs2/drivers/mainboard_efs2.py
     done
