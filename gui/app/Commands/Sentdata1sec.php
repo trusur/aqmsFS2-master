c1fa8f5cbb63bb9d29929a2c3e1f33db8d6719bf
ir001 <irwanantonio2708@gmail.com>
2024-06-24 10:52:12 +0700

    refactor: sending 1 sec data

--- gui/app/Commands/Sentdata1sec.php
+++ gui/app/Commands/Sentdata1sec.php
@@ -12,6 +12,7 @@ use App\Models\m_parameter;
 use App\Models\m_sensor_value;
 use CodeIgniter\CLI\BaseCommand;
 use CodeIgniter\CLI\CLI;
+use Exception;
 
 class Sentdata1sec extends BaseCommand
 {
@@ -82,81 +83,93 @@ class Sentdata1sec extends BaseCommand
 	public function run(array $params)
 	{
 		$is_sentto_trusur = @$this->configurations->where("name", "is_sentto_trusur")->first()->content ?? "1";
-		if ($is_sentto_trusur == "1") {
-			$trusur_api_server = @$this->configurations->where("name", "trusur_api_server")->first()->content ?? "";
-			$lastPutData = @$this->logSent->where(["is_sent_cloud" => 0, "time_group !=" => null])->orderBy("id")->first()->time_group;
-			if ($lastPutData) {
-				$measurement_ids = [];
-				$this->lastPutData = $lastPutData;
-				$is_exist = true;
-				$time_groups = $this->logSent->select("time_group")->where("is_sent_cloud = 0 and time_group >= '{$lastPutData}'")->groupBy("time_group")->findAll(50);
-				//print_r($time_groups);
-				$idStation = @$this->configurations->where("name", "id_stasiun")->first()->content ?? null;
-				$timeGroup = [];
-				foreach ($time_groups as $key => $time_group) {
-					$timeGroup[] = $time_group->time_group;
-					$arr[$key]["id_stasiun"] = $idStation;
-					$arr[$key]["waktu"] = $time_group->time_group;
-					$measurements = @$this->logSent->where(["time_group" => $time_group->time_group, "is_sent_cloud" => 0])->orderBy("id")->findAll(1000);
-					foreach ($measurements as $measurement) {
-						$parameter = @$this->parameters->select("code,p_type")->where(["id" => $measurement->parameter_id])->first();
-						$arr[$key][$parameter->code] = $measurement->value;
-						if($measurement->sub_avg_id){
-							$arr[$key]["sub_avg_id"] = $measurement->sub_avg_id;
-						}
-						if ($parameter->p_type == "particulate" || $parameter->p_type == "gas") {
-							$arr[$key]["stat_{$parameter->code}"] = $measurement->is_valid;
-						}
-						//$measurement_ids[] = $measurement->id;
-					}
-				} // end foreach
-				if ($is_exist) {
-                    // Sent to Server
-                    $trusur_api_username = @$this->configurations->where("name", "trusur_api_username")->first()->content ?? "";
-                    $trusur_api_password = @$this->configurations->where("name", "trusur_api_password")->first()->content ?? "";
-                    $trusur_api_key = @$this->configurations->where("name", "trusur_api_key")->first()->content ?? "";
-                    $data = json_encode($arr);
-                    $curl = curl_init();
-                    curl_setopt_array($curl, array(
-                        CURLOPT_URL => "https://" . $trusur_api_server . "/api/put_data_sec.php",
-                        CURLOPT_RETURNTRANSFER => true,
-                        CURLOPT_ENCODING => "",
-                        CURLOPT_MAXREDIRS => 10,
-                        CURLOPT_TIMEOUT => 30,
-                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                        CURLOPT_CUSTOMREQUEST => "PUT",
-                        CURLOPT_USERPWD => $trusur_api_username . ":" . $trusur_api_password,
-                        CURLOPT_POSTFIELDS => $data,
-                        CURLOPT_HTTPHEADER => array(
-                            "Api-Key: " . $trusur_api_key,
-                            "cache-control: no-cache",
-                            "content-type: application/json"
-                        ),
-                        CURLOPT_SSL_VERIFYPEER => 0, //skip SSL Verification | disable SSL verify peer
-                    ));
-    
-                    $response = curl_exec($curl);
-                    $err = curl_error($curl);
-                    
-					CLI::write($response, "green");
-					CLI::write("\n");
-    
-                    curl_close($curl);
-    
-                    if ($err) {
-                        echo "cURL Error #:" . $err;
-                    } 
-					if (strpos(" " . $response, "success") > 0) {
-						$this->logSent->whereIn("time_group", $timeGroup)->delete();
-					} else {
-						echo $response;
-					}
+		$is_sentto_trusur = (int) get_config("is_sentto_trusur", 1);
+		if($is_sentto_trusur == 0) return;
 
+		$trusur_api_server = get_config("trusur_api_server");
+		$lastSent = $this->getLastSent();
+		if ($lastSent) {
+			$is_exist = false;
+			$time_groups = $this->logSent->select("time_group")->where("is_sent_cloud = 0 and time_group >= '{$lastSent}'")->groupBy("time_group")->findAll(60);
 
-					
-                }
+			$idStation = get_config("id_station");
+			$timeGroup = [];
+			foreach ($time_groups as $key => $time_group) {
+				$timeGroup[] = $time_group->time_group;
+				$arr[$key]["id_stasiun"] = $idStation;
+				$arr[$key]["waktu"] = $time_group->time_group;
+				$measurements = $this->logSent->where(["time_group" => $time_group->time_group, "is_sent_cloud" => 0])->orderBy("id")->findAll(500);
+				foreach ($measurements as $measurement) {
+					$parameter = $this->parameters->select("code,p_type")->where(["id" => $measurement->parameter_id])->first();
+					if(empty($parameter)) continue;
+
+					$arr[$key][$parameter->code] = $measurement->value;
+					if($measurement->sub_avg_id){
+						$arr[$key]["sub_avg_id"] = $measurement->sub_avg_id;
+					}
+					if ($parameter->p_type == "particulate" || $parameter->p_type == "gas") {
+						$arr[$key]["stat_{$parameter->code}"] = $measurement->is_valid;
+					}
+				}
 			}
 
+			$is_exist = count($arr) > 0; 
+
+			if ($is_exist) {
+				// Sent to Server
+				$trusur_api_username = get_config("trusur_api_username");
+				$trusur_api_password = get_config("trusur_api_password");
+				$trusur_api_key = get_config("trusur_api_key");
+				$data = json_encode($arr);
+				$curl = curl_init();
+				curl_setopt_array($curl, array(
+					CURLOPT_URL => "https://" . $trusur_api_server . "/api/put_data_sec.php",
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_ENCODING => "",
+					CURLOPT_MAXREDIRS => 10,
+					CURLOPT_TIMEOUT => 30,
+					CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+					CURLOPT_CUSTOMREQUEST => "PUT",
+					CURLOPT_USERPWD => $trusur_api_username . ":" . $trusur_api_password,
+					CURLOPT_POSTFIELDS => $data,
+					CURLOPT_HTTPHEADER => array(
+						"Api-Key: " . $trusur_api_key,
+						"cache-control: no-cache",
+						"content-type: application/json"
+					),
+					CURLOPT_SSL_VERIFYPEER => 0, //skip SSL Verification | disable SSL verify peer
+				));
+
+				$response = curl_exec($curl);
+				$err = curl_error($curl);
+				
+				CLI::write($response, "green");
+				CLI::write("\n");
+
+				curl_close($curl);
+
+				if ($err) {
+					echo "cURL Error #:" . $err;
+				} 
+				if (strpos(" " . $response, "success") > 0) {
+					$this->logSent->whereIn("time_group", $timeGroup)->delete();
+				} else {
+					echo $response;
+				}
+			}
+		}
+	}
+
+	public function getLastSent(){
+		$twoMinAgo = date("Y-m-d H:i:s", strtotime("-2 minutes"));
+		try{
+			$logSent = $this->logSent->where("is_sent_cloud = 1")->orderBy("id", "desc")->first();
+			if(empty($logSent)){
+				return $twoMinAgo;
+			}
+			return $logSent->time_group;
+		}catch(Exception $e){
+			return $twoMinAgo;
 		}
 	}
 }
