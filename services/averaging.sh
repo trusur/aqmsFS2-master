19278aebbe75823078cb0bd56bbfd815fbcfb827
ir001 <irwanantonio2708@gmail.com>
2024-06-24 19:27:29 +0700

    debug avg

--- services/averaging.sh
+++ services/averaging.sh
@@ -3,6 +3,6 @@ while true
     do
         # Replace gui with full path to spark folder
         /usr/bin/php /home/mx/aqms-efs2/gui/spark command:formula_measurement_logs
-        /usr/bin/php /home/mx/aqms-efs2/gui/spark command:avg1min
-        /usr/bin/php /home/mx/aqms-efs2/gui/spark command:avg30min
+        # /usr/bin/php /home/mx/aqms-efs2/gui/spark command:avg1min
+        # /usr/bin/php /home/mx/aqms-efs2/gui/spark command:avg30min
     done
