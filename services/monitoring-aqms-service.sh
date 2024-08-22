4222390393b2131fa3ab0c446ac09872d5bbf4ec
ir001 <irwanantonio2708@gmail.com>
2024-08-22 11:32:50 +0700

    fix: monitoring aqms

--- services/monitoring-aqms-service.sh
+++ services/monitoring-aqms-service.sh
@@ -5,8 +5,8 @@ app_name="AQMS_EFS2"
 while true; do
   if ! pgrep -x "$app_name" > /dev/null; then
     echo "Application $app_name closed"
-    systemctl stop aqms-averaging
-    systemctl stop aqms-driver-alpha
+    echo mx | sudo -S systemctl stop aqms-averaging
+    echo mx | sudo -S systemctl stop aqms-driver-alpha
     echo "AQMS services stopped" 
     break
   fi
