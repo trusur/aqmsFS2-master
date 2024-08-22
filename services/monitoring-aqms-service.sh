125af30e67df74a758c72851c029da8f432b5e64
ir001 <irwanantonio2708@gmail.com>
2024-08-22 13:37:34 +0700

    fix: run aqms

--- services/monitoring-aqms-service.sh
+++ services/monitoring-aqms-service.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 app_name="AQMS_EFS2"
-sleep 60s
+echo "Do not close this terminal. It will close automatically when $app_name is closed"
+sleep 5s
 while true; do
   if ! pgrep -x "$app_name" > /dev/null; then
     echo "Application $app_name closed"
