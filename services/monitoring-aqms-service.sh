373ec3d63e47e6705700c96484bad9f5f35c994b
ir001 <irwanantonio2708@gmail.com>
2024-08-27 19:29:39 +0700

    fix: stop aqms-sending

--- services/monitoring-aqms-service.sh
+++ services/monitoring-aqms-service.sh
@@ -7,6 +7,7 @@ while true; do
   if ! pgrep -x "$app_name" > /dev/null; then
     echo "Application $app_name closed"
     echo mx | sudo -S systemctl stop aqms-averaging
+    echo mx | sudo -S systemctl stop aqms-sending
     echo mx | sudo -S systemctl stop aqms-driver-alpha
     echo "AQMS services stopped"
     break
