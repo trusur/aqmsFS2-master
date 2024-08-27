bfe3c9d9ae1b26476f934292b13b43b60f9cbb13
ir001 <irwanantonio2708@gmail.com>
2024-08-27 19:28:45 +0700

    fix: service sending data

--- services/sending.sh
+++ services/sending.sh
@@ -5,6 +5,6 @@ while true
         /usr/bin/php  /home/mx/aqms-efs2/gui/spark command:sentdata # Sending {interval}min data to Trusur Server
         /usr/bin/php  /home/mx/aqms-efs2/gui/spark command:sentdata1min # Sending 1min data to Trusur Server
         /usr/bin/php  /home/mx/aqms-efs2/gui/spark command:sentdata1sec # Sending 1sec data to Trusur Server
-        # /usr/bin/php  /home/mx/aqms-efs2/gui/spark command:sentdata_klhk # Sending {interval}min data  to KLHK Server
-        sleep 10 # Sleep every 30 second
+        /usr/bin/php  /home/mx/aqms-efs2/gui/spark command:sentdata_klhk # Sending {interval}min data  to KLHK Server
+        sleep 10s # Sleep every 30 second
     done
