7671fdae339deed98105f9521c281d6f9f4d36f2
affandi-cetta <affandi@cetta.tech>
2024-11-21 17:46:18 +0700

    Update AQMS_EFS2.sh

--- AQMS_EFS2.sh
+++ AQMS_EFS2.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-echo "Starting ISPUTEK EFS2"
+echo "Waiting 5 minutes before running AQMS"
+date
+echo "Will start AQMS at:"
+date -d "+5 minutes"
+sleep 5m
+echo "5 minutes have passed.Starting ISPUTEK EFS2"
+date
 clear
 cd ~/aqms-efs2/ && /usr/bin/python3 AQMS_EFS2.py
