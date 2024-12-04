ebc0fbd9bca1f4027254bbcbe29b8c77ff6a1e47
affandi-cetta <affandi@cetta.tech>
2024-12-04 11:54:25 +0700

    Update test_modbus_hc.py

--- drivers/test_modbus_hc.py
+++ drivers/test_modbus_hc.py
@@ -5,12 +5,13 @@ client = ModbusClient(port='/dev/ttyUSB1', baudrate=9600, stopbits=1, parity='N'
 
 # Tentukan alamat perangkat dan register yang ingin dibaca
 device_address = 1  # Address perangkat (0x01)
-registers = 40041  # PM2.5, PM10, CO (contoh)
+registers = [40041]  # PM2.5, PM10, CO (contoh)
 
 # Fungsi untuk membaca data
 def read_registers():
     for reg in registers:
         # Gunakan parameter 'slave' atau 'address' untuk menentukan alamat perangkat
+        result = client.read_holding_registers(0X01, 2, address=device_address)
         if result.isError():
             print(f"Error membaca register {reg}")
         else:
