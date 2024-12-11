f37c5ddedc2b1713841c3034b2fbac9d52d3bb26
affandi-cetta <affandi@cetta.tech>
2024-12-11 22:02:10 +0700

    Revert "Update test_modbus_hc.py"
    This reverts commit 8e11fd95d1f41ea165ca1a42292b06b150ef3573.

--- drivers/test_modbus_hc.py
+++ drivers/test_modbus_hc.py
@@ -38,14 +38,16 @@ def main():
         print("Connected to Modbus device")
 
         # Membaca sensor PM2.5 dan PM10 (alamat register dari manual)
-        hc_address = 40  # Alamat register untuk PM2.5 (40021 - 40001)
+        pm25_address = 20  # Alamat register untuk PM2.5 (40021 - 40001)
+        pm10_address = 22  # Alamat register untuk PM10 (40023 - 40001)
 
+        pm25_value = read_register(client, pm25_address)
+        pm10_value = read_register(client, pm10_address)
 
-        hc_value = read_register(client, hc_address)
-
-
-        if hc_value is not None:
-            print(f"PM2.5: {hc_value} µg/m³")
+        if pm25_value is not None:
+            print(f"PM2.5: {pm25_value} µg/m³")
+        if pm10_value is not None:
+            print(f"PM10: {pm10_value} µg/m³")
 
         # Tutup koneksi Modbus
         client.close()
