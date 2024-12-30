3cd4284b9c3911d8168af64f812e0fecc7dd1a80
affandi-cetta <affandi@cetta.tech>
2024-12-31 00:21:38 +0700

    Check DM data

--- AQMS_EFS2.py
+++ AQMS_EFS2.py
@@ -11,22 +11,70 @@ def is_process_running(pid):
     except psutil.NoSuchProcess:
         return False
 def exit_handler():
+    # Stoping AQMS Driver
     print("Stopping AQMS Driver Service...\n")
     subprocess.Popen("echo mx | sudo -S systemctl stop aqms-driver-alpha", shell=True)
+    print("AQMS Driver should be stopped...")
     time.sleep(1)
+    
+    # Stopping AQMS Averaging
     print("Stopping AQMS Averaging Service...\n")
     subprocess.Popen("echo mx | sudo -S systemctl stop aqms-averaging", shell=True)
-    print("AQMS should be stopped...")
+    print("AQMS Averaging should be stopped...")
+    time.sleep(1)
+
+    # Stopping AQMS Sending
+    print("Stopping AQMS Sending Service...\n")
+    subprocess.Popen("echo mx | sudo -S systemctl stop aqms-sending", shell=True)
+    print("AQMS Sending should be stopped...")
+    time.sleep(1)
+
+    # Stopping AQMS HC
+    print("Stopping AQMS HC Service...\n")
+    subprocess.Popen("echo mx | sudo -S systemctl stop aqms-hc", shell=True)
+    print("AQMS HC should be stopped...")
+    time.sleep(1)
+
+    # Stopping AQMS DM
+    print("Stopping AQMS DM Service...\n")
+    subprocess.Popen("echo mx | sudo -S systemctl stop aqms-dm", shell=True)
+    print("AQMS HC should be stopped...")
+    time.sleep(1)
+    
     file = open('test.txt','w')
     file.write('1')
     file.close()
 def exit_handler_signal(signum,frame):
+    # Stoping AQMS Driver
     print("Stopping AQMS Driver Service...\n")
-    # subprocess.Popen("echo mx | sudo -S systemctl stop aqms-driver-alpha", shell=True)
+    subprocess.Popen("echo mx | sudo -S systemctl stop aqms-driver-alpha", shell=True)
+    print("AQMS Driver should be stopped...")
     time.sleep(1)
+    
+    # Stopping AQMS Averaging
     print("Stopping AQMS Averaging Service...\n")
-    # subprocess.Popen("echo mx | sudo -S systemctl stop aqms-averaging", shell=True)
-    print("AQMS should be stopped...")
+    subprocess.Popen("echo mx | sudo -S systemctl stop aqms-averaging", shell=True)
+    print("AQMS Averaging should be stopped...")
+    time.sleep(1)
+
+    # Stopping AQMS Sending
+    print("Stopping AQMS Sending Service...\n")
+    subprocess.Popen("echo mx | sudo -S systemctl stop aqms-sending", shell=True)
+    print("AQMS Sending should be stopped...")
+    time.sleep(1)
+
+    # Stopping AQMS HC
+    print("Stopping AQMS HC Service...\n")
+    subprocess.Popen("echo mx | sudo -S systemctl stop aqms-hc", shell=True)
+    print("AQMS HC should be stopped...")
+    time.sleep(1)
+
+    # Stopping AQMS DM
+    print("Stopping AQMS DM Service...\n")
+    subprocess.Popen("echo mx | sudo -S systemctl stop aqms-dm", shell=True)
+    print("AQMS HC should be stopped...")
+    time.sleep(1)
+    
     file = open('test.txt','w')
     file.write('1')
     file.close()
@@ -57,29 +105,42 @@ def init_pump():
         cnx.close()
     except Exception as e:
         print('Init Pump Error: ',e)
+        
 # print("Starting Pump...")
 # init_pump()
 truncate_sensor_values()
 time.sleep(1)
+
+# Running AQMS DRIVER
 print("Checking AQMS Driver Service...\n")
 subprocess.Popen("echo mx | sudo -S systemctl restart aqms-driver-alpha", shell=True)
 time.sleep(1)
+
+# Running AQMS HC
 print("Checking AQMS HC Service...\n")
 subprocess.Popen("echo mx | sudo -S systemctl restart aqms-hc", shell=True)
 time.sleep(1)
+
+# Running AQMS Averaging
 print("Checking AQMS Averaging Service...\n")
 subprocess.Popen("echo mx | sudo -S systemctl restart aqms-averaging", shell=True)
 time.sleep(1)
-print("Checking AQMS HC Sending...\n")
+
+# Running AQMS Sending
+print("Checking AQMS  Sending...\n")
 subprocess.Popen("echo mx | sudo -S systemctl restart aqms-sending", shell=True)
 time.sleep(1)
+
+# Check CronJob
 print("Checking Cronjob Service...\n")
 subprocess.Popen("echo mx | sudo -S crontab -l", shell=True)
-time.sleep(1)
+time.sleep(2)
+
 print("Starting Web Server...")
 subprocess.Popen("php gui/spark serve", shell=True, stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT)
 print("Ready..")
-time.sleep(1)
+time.sleep(2)
+
 print("Trying to open application...")
 subprocess.Popen("firefox --kiosk=http://localhost:8080", shell=True)
 print("CTRL+C to exit")
