9d1c084f532663d06a389393c126f0be2ec78cdb
affandi-cetta <affandi@cetta.tech>
2025-01-09 20:21:02 +0700

    Update testing.php

--- gui/app/Commands/testing.php
+++ gui/app/Commands/testing.php
@@ -3,6 +3,7 @@
 namespace App\Commands;
 
 use App\Models\m_configuration;
+use App\Models\m_log_sent;
 use App\Models\m_measurement;
 use App\Models\m_measurement_1min;
 use App\Models\m_measurement_log;
@@ -28,6 +29,7 @@ class testing extends BaseCommand
 	protected $measurements;
 	protected $configurations;
 	protected $lastPutData;
+	protected $MLogSent;
 
 	public function __construct()
 	{
@@ -35,8 +37,9 @@ class testing extends BaseCommand
 		$this->sensor_values =  new m_sensor_value();
 		$this->measurement_logs =  new m_measurement_log();
 		$this->configurations =  new m_configuration();
-		$this->measurements =  new m_measurement_1min();
+		$this->measurements =  new m_measurement();
 		$this->lastPutData = "0000-00-00 00:00";
+		$this->MLogSent = new m_log_sent();
 	}
 	/**
 	 * The Command's Name
@@ -81,82 +84,81 @@ class testing extends BaseCommand
 	public function run(array $params)
 	{
 		$is_sentto_trusur = @$this->configurations->where("name", "is_sentto_trusur")->first()->content ?? "1";
-		$minute = date("i") > 30 ? "30" : "00";
-		$endAt = date("Y-m-d H:$minute:00");
 		if ($is_sentto_trusur == "1") {
 			$trusur_api_server = @$this->configurations->where("name", "trusur_api_server")->first()->content ?? "";
-			$lastPutData = @$this->measurements->where(["is_sent_cloud" => 0])->orderBy("id")->first()->time_group;
-			if ($lastPutData) {
-				$measurement_ids = [];
-				$this->lastPutData = $lastPutData;
-				$is_exist = true;
-				$time_groups = $this->measurements->select("time_group")->where("is_sent_cloud = 0 and time_group < '{$endAt}'")->groupBy("time_group")->findAll();
-				$idStation = @$this->configurations->where("name", "id_stasiun")->first()->content ?? null;
-				foreach ($time_groups as $time_group) {
-					$arr["id_stasiun"] = $idStation;
-					$arr["waktu"] = $time_group->time_group;
-					$measurements = @$this->measurements->where(["time_group" => $time_group->time_group, "is_sent_cloud" => 0])->orderBy("id")->findAll(500);
+			$measurement_ids = "";
+			$arr["id_stasiun"] = @$this->configurations->where("name", "id_stasiun")->first()->content ?? null;
+
+			$time_groups = @$this->measurements->select("time_group")->where(["is_sent_cloud" => 0])->groupBy("time_group")->orderBy("id")->findAll();
+			foreach ($time_groups as $timeGroup) {
+				$time_group = $timeGroup->time_group;
+				CLI::write("Sent data {$time_group} to Trusur Server", "green");
+				$is_exist = false;
+				if ($time_group) {
+					$is_exist = true;
+					$arr["waktu"] = $time_group;
+					$measurements = @$this->measurements->where(["time_group" => $time_group, "is_sent_cloud" => 0])->orderBy("id")->findAll();
 					foreach ($measurements as $measurement) {
-						$parameter = @$this->parameters->select("code,p_type")->where(["id" => $measurement->parameter_id])->first();
+						$parameter = @$this->parameters->where(["id" => $measurement->parameter_id])->first();
 						$arr[$parameter->code] = $measurement->value;
+						if ($measurement->avg_id) {
+							$arr["avg_id"] = $measurement->avg_id;
+						}
 						if ($parameter->p_type == "particulate" || $parameter->p_type == "gas") {
 							$arr["stat_{$parameter->code}"] = $measurement->is_valid;
 							$arr["total_{$parameter->code}"] = (float) $measurement->total_data;
 							$arr["valid_{$parameter->code}"] = (float) $measurement->total_valid;
 						}
-						$measurement_ids[] = $measurement->id;
-						$arr["avg_id"] = (int) $measurement->avg_id;
-						$arr["sub_avg_id"] = $measurement->sub_avg_id;
+						$measurement_ids .= $measurement->id . ",";
 					}
+				}
 
-					$arr['waktu'] = (new DateTime($arr['waktu'], new DateTimeZone('Asia/Jakarta')))
-						->setTimezone(new DateTimeZone('UTC'))
-						->format('Y-m-d\TH:i:s.v\Z');
-					$arr["tipe_stasiun"] = "lowcost";
-					$arr['sta_lat'] = "";
-					$arr['sta_lon'] = "";
-					unset($arr['sub_avg_id']);
+				$measurement_ids = substr($measurement_ids, 0, -1);
 
+				$arr["tipe_stasiun"] = "lowcost";
+				$arr['sta_lat'] = "";
+				$arr['sta_lon'] = "";
+				$arr['waktu'] = (new DateTime($arr['waktu'], new DateTimeZone('Asia/Jakarta')))
+					->setTimezone(new DateTimeZone('UTC'))
+					->format('Y-m-d\TH:i:s.v\Z');
+				unset($arr['sub_avg_id']);
+				unset($arr['avg_id']);
 
-					// SENDING DATA TO GREENTEAMS
-					try {
-						$client_url = getenv('CLIENT_API_URL');
-						$client_key = getenv('CLIENT_API_KEY');
-						$new_arr = [$arr];
-						$data = json_encode($new_arr);
-						$curl = curl_init();
-						curl_setopt_array($curl, array(
-							CURLOPT_URL => $client_url,
-							CURLOPT_RETURNTRANSFER => true,
-							CURLOPT_ENCODING => "",
-							CURLOPT_MAXREDIRS => 10,
-							CURLOPT_TIMEOUT => 30,
-							CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-							CURLOPT_CUSTOMREQUEST => "POST",
-							CURLOPT_POSTFIELDS => $data,
-							CURLOPT_HTTPHEADER => array(
-								"CLIENT-API-KEY: " . $client_key,
-								"cache-control: no-cache",
-								"content-type: application/json"
-							),
-							CURLOPT_SSL_VERIFYPEER => 0, // Disable SSL peer verification (use with caution)
-						));
+				$new_arr = [$arr];
+				
+				// SENDING DATA TO GREENTEAMS
+				try {
+					$client_url = getenv('CLIENT_API_URL_ISPU');
+					$client_key = getenv('CLIENT_API_KEY');
 
-						$response = curl_exec($curl);
-
-						if (curl_errno($curl)) {
-							echo 'cURL Error: ' . curl_error($curl);
-						} else {
-							echo json_encode($response);
-						}
-
+					$data = json_encode($new_arr);
+					$curl = curl_init();
+					curl_setopt_array($curl, array(
+						CURLOPT_URL => $client_url,
+						CURLOPT_RETURNTRANSFER => true,
+						CURLOPT_ENCODING => "",
+						CURLOPT_MAXREDIRS => 10,
+						CURLOPT_TIMEOUT => 30,
+						CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+						CURLOPT_CUSTOMREQUEST => "POST",
+						CURLOPT_POSTFIELDS => $data,
+						CURLOPT_HTTPHEADER => array(
+							"CLIENT-API-KEY: " . $client_key,
+							"cache-control: no-cache",
+							"content-type: application/json"
+						),
+						CURLOPT_SSL_VERIFYPEER => 0,
+					));
+					curl_exec($curl);
+					if (curl_errno($curl)) {
+						echo 'cURL Error: ' . curl_error($curl);
+					}
+					curl_close($curl);
+				} catch (Exception $e) {
+					echo "Error: " . $e->getMessage();
+				} finally {
+					if (isset($curl)) {
 						curl_close($curl);
-					} catch (Exception $e) {
-						echo "Error: " . $e->getMessage();
-					} finally {
-						if (isset($curl)) {
-							curl_close($curl);
-						}
 					}
 				}
 			}
